8e8b2e11b4074e8405e7747e2b7955ca6de965bf
ahsanjamali <ahsanjamali7042@gmail.com>
2024-09-15 15:46:50 +0500

    Complete Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+let symbol = "X";
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  // const [symbol, setSymbol] = useState("X");
+
+  function handlePrevious() {
+    if (step > 1) setStep((currStep) => currStep - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep((currStep) => currStep + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button
+        className="close"
+        onClick={() => {
+          setIsOpen((is) => !is);
+          if (symbol === "X") symbol = "✓";
+          else symbol = "X";
+        }}
+      >
+        {symbol}
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <MessageStep step={step}> {messages[step - 1]} </MessageStep>
+
+          <div className="buttons">
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+              <span>👈</span> Previous
+            </Button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+              Next<span>👉</span>
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
-export default App;
+function MessageStep({ step, children }) {
+  return (
+    <p className="message">
+      Step {step}: {children}
+    </p>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
